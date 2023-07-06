437c2e4a41c91a847333f4460bc31fcc278274c3
star <juniorprogrammer09@gmail.com>
2023-07-07 01:12:38 +0300

    change ID type to Long

--- src/main/java/com/junrdev/rds/repo/TodoRepository.java
+++ src/main/java/com/junrdev/rds/repo/TodoRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface TodoRepository extends JpaRepository<Todo, String> {
+public interface TodoRepository extends JpaRepository<Todo, Long> {
 }
