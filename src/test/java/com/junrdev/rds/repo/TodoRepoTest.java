ac7de1ead151587e9990e9821589830540caa273
star <juniorprogrammer09@gmail.com>
2023-07-07 01:19:00 +0300

    initial test

---
+++ src/test/java/com/junrdev/rds/repo/TodoRepoTest.java
@@ -0,0 +1,19 @@
+package com.junrdev.rds.repo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class TodoRepoTest {
+
+    private final TodoRepository repository;
+
+    @Autowired
+    public TodoRepoTest(TodoRepository repository) {
+        this.repository = repository;
+    }
+
+    @Test
+    void contextLoad(){}
+}
